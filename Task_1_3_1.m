8faa94a5c3fa7b1bec908a796184fae4133aff2e
Mattk <Mattk@Mattdell.dhcp.wustl.edu>
2022-11-16 17:01:13 -0600

    hi

--- Task_1_3_1.m
+++ Task_1_3_1.m
@@ -5,7 +5,7 @@ y = [0 0 0 0 0];
 theta2 = [0 0 0 0 0];
 Ray1 = [x1; theta1; y; theta2];
 Ray2 = [x2; theta1; y; theta2];
-d = .5;
+d = .3;
 Md = [1 d 0 0;
       0 1 0 0;
       0 0 1 d;
@@ -16,7 +16,9 @@ ray_z1 = [zeros(1,size(Ray1,2)); d*ones(1,size(Ray1,2))];
 ray_z2 = [zeros(1,size(Ray2,2)); d*ones(1,size(Ray2,2))];
 figure
 hold on
-plot(ray_z1, [Ray1(1,:); Z1(1,:)]);
-plot(ray_z2, [Ray2(1,:); Z2(1,:)]);
+plot1 = plot(ray_z1, [Ray1(1,:); Z1(1,:)]);
+plot2 = plot(ray_z2, [Ray2(1,:); Z2(1,:)]);
 hold off
+set(plot1,'color','r');
+set(plot2,'color','b');
 
