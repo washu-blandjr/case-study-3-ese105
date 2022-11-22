823d1d99d2353b0ba534f1c927a039c6cb57bc71
Mattk <Mattk@Mattdell>
2022-11-22 14:43:41 -0600

    HI

--- Task_2_2_2.m
+++ Task_2_2_2.m
@@ -1,12 +1,13 @@
  load("lightField.mat");
-d1 = .5;
-d2 = .5;
+d1 = .250;
+d2 = -1.003;
 f = ((1./d1)+(1./d2)).^-1;
 
+
 Md1 = [1 d1 0 0;
-      0 1  0 0;
-      0 0  1 d1;
-      0 0  0 1];
+       0 1  0 0;
+       0 0  1 d1;
+       0 0  0 1];
 
 Md2 = [1 d2 0 0;
        0 1  0 0;
@@ -22,5 +23,6 @@ Z1 = Md1*rays;
 Z2 = Mf*rays;
 Z3 = Md2*rays;
 
-output =  rays2img(Z3(1,:),Z3(3,:),.025,500);
+output =  rays2img(Z3(1,:),Z3(3,:),.005,500);
+figure;
 imshow(output);
