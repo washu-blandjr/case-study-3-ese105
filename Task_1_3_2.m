2eb5b5499df7511bcad61e0f9f5c4c0e233f0884
washu-blandjr <b.carmen@wustl.edu>
2022-11-21 19:43:39 -0600

    done with code task, will optimize later

--- Task_1_3_2.m
+++ Task_1_3_2.m
@@ -4,11 +4,11 @@ load 'lightField.mat';
 % -optimize code to run in a few lines
 %% #2 Manipulating camera sensor
 
-output1 = rays2img(rays(1,:),rays(3,:),.005,200); % control 
-output2 = rays2img(rays(1,:),rays(3,:),.0005,200); % decrease sensor width
-output3 = rays2img(rays(1,:),rays(3,:),.05,200); % increase sensor width
-output4 = rays2img(rays(1,:),rays(3,:),.005,100); % decrease pixel #
-output5 = rays2img(rays(1,:),rays(3,:),.005,500); %increase pixel #
+[output1, x1, y1] = rays2img(rays(1,:),rays(3,:),.005,200);  % control 
+[output2, x2, y2] = rays2img(rays(1,:),rays(3,:),.0001,200); % decrease sensor width
+[output3, x3, y3] = rays2img(rays(1,:),rays(3,:),.015,200);  % increase sensor width
+[output4, x4, y4] = rays2img(rays(1,:),rays(3,:),.005,100);  % decrease pixel #
+[output5, x5, y5] = rays2img(rays(1,:),rays(3,:),.005,500);  % increase pixel #
 figure;
 imshow(output1);
 figure;
@@ -29,25 +29,25 @@ Md = [1 d 0 0;
 
 rays_z = Md*rays;
 figure;
-outputz1 = rays2img(rays_z(1,:),rays_z(3,:),.005,200); % control
+[outputz1, xz1, yz1] = rays2img(rays_z(1,:),rays_z(3,:),.005,200); % control
 imshow(outputz1);
 
-d = 0.00003; % decrease dist
+d = 0.003; % decrease dist
 Md = [1 d 0 0;
       0 1 0 0;
       0 0 1 d;
       0 0 0 1];
 rays_z = Md*rays;
 figure;
-outputz2 = rays2img(rays_z(1,:),rays_z(3,:),.005,200); 
+[outputz2, xz2, yz2] = rays2img(rays_z(1,:),rays_z(3,:),.005,200); 
 imshow(outputz2);
 
-d = 25.03; % increase dist
+d = 10; % increase dist
 Md = [1 d 0 0;
       0 1 0 0;
       0 0 1 d;
       0 0 0 1];
 rays_z = Md*rays;
 figure;
-outputz3 = rays2img(rays_z(1,:),rays_z(3,:),.005,200); 
+[outputz3, xz3, yz3] = rays2img(rays_z(1,:),rays_z(3,:),.005,200); 
 imshow(outputz3);
